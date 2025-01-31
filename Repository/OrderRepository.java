6ac11c59f3a55114fdb6b7b303fa389321ceae1b
prav-unique <167164339+prav-unique@users.noreply.github.com>
2025-01-31 11:10:34 +0530

    Create OrderRepository.java

---
+++ Repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.foodDelivery.project.Repository;
+
+import com.foodDelivery.project.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepo extends JpaRepository <Order,Integer>{
+
+}
