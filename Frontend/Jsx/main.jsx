709033a3632ca7117ddc67b26f3ad16d3e8db3c4
lakshu2817 <161627844+lakshu2817@users.noreply.github.com>
2025-02-18 01:15:55 +0530

    Create main.jsx

---
+++ Frontend/Jsx/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import StoreContextProvider from './context/StoreContext.jsx'
+import Restaurant from './pages/Restaurant/Restaurant.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <StoreContextProvider>
+      <App />
+      
+    </StoreContextProvider>
+  </BrowserRouter>
+
+
+)
