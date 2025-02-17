df9287f633da7e97d1529f9c1e32b6f19fe277cf
lakshu2817 <161627844+lakshu2817@users.noreply.github.com>
2025-02-18 01:12:27 +0530

    Create Header.jsx

---
+++ Frontend/Jsx/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order Your Favourite Food</h2>
+                <p>Choose from a diverse menu featuring a delectable dishes crafted with the finest dished crafted with the ingredients and culinary expertise. Our mission is to satisfy your cravings and taste buds.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
